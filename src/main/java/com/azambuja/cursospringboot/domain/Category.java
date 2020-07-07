a6fefcc0d4c4987f5cdac5336fbf5ad1b42ebb16
Ygor Azambuja <ygorazambuja@gmail.com>
2020-07-07 12:02:05 -0400

    h2 connection and automatic database creation

---
+++ src/main/java/com/azambuja/cursospringboot/domain/Category.java
@@ -0,0 +1,57 @@
+package com.azambuja.cursospringboot.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Category implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    public Category(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Category() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Category category = (Category) o;
+
+        return id.equals(category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
--- src/main/java/com/azambuja/cursospringboot/resources/CategoryResource.java
+++ src/main/java/com/azambuja/cursospringboot/resources/CategoryResource.java
@@ -1,12 +1,25 @@
 package com.azambuja.cursospringboot.resources;
 
+import com.azambuja.cursospringboot.domain.Category;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
     @RequestMapping( method = RequestMethod.GET)
-    public String list() {
-        return "Rest Working";
+    public List<Category> list() {
+
+        Category category1 = new Category(1, "Informatica");
+        Category category2 = new Category(2, "Escritório");
+
+        List<Category> list = new ArrayList<>();
+        list.add(category1);
+        list.add(category2);
+
+
+        return list;
     }
 }
