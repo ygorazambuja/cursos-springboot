8f426be055824d3fd1f175f1276a0bb844698ff7
Ygor Azambuja <ygorazambuja@gmail.com>
2020-07-07 10:41:53 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Curso Java com Spring Boot
---
+++ src/main/java/com/azambuja/cursospringboot/CursoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.azambuja.cursospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursoSpringbootApplication.class, args);
+    }
+
+}
